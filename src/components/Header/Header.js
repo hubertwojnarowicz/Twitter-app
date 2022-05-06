ae1a39e6f6fade562a3c73214c941db87b49bef3
Hubert <hubertwojnarowicz01@gmail.com>
2022-05-06 12:00:55 +0200

    Initialize app + add css reset and variables

---
+++ src/components/Header/Header.js
@@ -0,0 +1,3 @@
+export default function Header() {
+  return <h2>Header</h2>;
+}
---
+++ src/constants.js
@@ -0,0 +1,20 @@
+export const COLORS = {
+  black: {
+    primary: '#0f1419',
+    classic: '#000000',
+  },
+  blue: {
+    primary: '#1d9bf0',
+    secondary: '#1a8cd8',
+    900: '#1d9bf0',
+  },
+  gray: {
+    primary: '#f7f9f9',
+    secondary: '#d6d9db',
+  },
+  white: {
+    classic: '#ffffff',
+    primary: '#e6e6e6',
+    secondary: '#e7e9ea',
+  },
+};
